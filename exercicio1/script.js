2af9002d92bc35696756fb28aaf2072bcac5d085
Vanessa Farias <vr.farias1992@gmail.com>
2022-08-31 14:50:40 -0300

    exercicios Strings-JS

---
+++ exercicio1/script.js
@@ -0,0 +1,6 @@
+const nome = prompt("Qual o seu nome?")
+const comida1 = prompt("Comida favorita 1")
+const comida2 = prompt("Comida favorita 2")
+const comida3 = prompt("Comida favorita 3")
+
+console.log(`Meu nome é ${nome}, e minhas comidas favoritas são: \n${comida1}; \n${comida2}; \n${comida3}.`)
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,9 @@
 //Crie a const para a frase aqui
+const minhaFrase = ("Jorge tem uma casa verde e com portão azul, com os dizeres: \"BOAS VINDAS, mas não deixe o gato sair\"")
+const novaFrase1 = minhaFrase.replace("verde", "rosa")
+const novaFrase2 = novaFrase1.replace("azul", "laranja")
+
+console.log(minhaFrase) //frase original
+console.log(novaFrase2) //frase modificada
+console.log(novaFrase2.includes("verde")) //imprime false
+console.log(novaFrase2.includes("laranja")) //imprime true
