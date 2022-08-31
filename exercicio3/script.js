0bde55ab7a6ebc8e0fa929642448df4c9ac92a2f
Vanessa Farias <vr.farias1992@gmail.com>
2022-08-31 15:17:07 -0300

    atualizaçoes

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -2,8 +2,10 @@
 const minhaFrase = ("Jorge tem uma casa verde e com portão azul, com os dizeres: \"BOAS VINDAS, mas não deixe o gato sair\"")
 const novaFrase1 = minhaFrase.replace("verde", "rosa")
 const novaFrase2 = novaFrase1.replace("azul", "laranja")
+const novafrase3 = "mas não deixe o gato sair".toUpperCase()
 
 console.log(minhaFrase) //frase original
 console.log(novaFrase2) //frase modificada
 console.log(novaFrase2.includes("verde")) //imprime false
 console.log(novaFrase2.includes("laranja")) //imprime true
+console.log(novaFrase2.replace("mas não deixe o gato sair", novafrase3))
